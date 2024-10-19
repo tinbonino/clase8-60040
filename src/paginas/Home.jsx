9e692433c716f24e99249162d187742551fd018a
martin <tinbonino@gmail.com>
2024-10-19 12:57:53 -0300

    Redeploy para ver actualizacion

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <div>
         <h1>Bienvenidos a mi página web</h1>
         <p>{import.meta.env.VITE_MENSAJE}</p>
-        <h2>Felicitaciones por llegar hasta acá y por comprometerse los sábados!</h2>
+        <h2>Felicitaciones por llegar hasta acá y por comprometerse los sábados eh!!</h2>
     </div>
   )
 }
